ba0f6d37b119f6c3f734b084bc5d323291fb119a
Brandon Goodman <bran99@gmail.com>
2020-11-19 22:41:38 -0500

    Assessment complete and styled (minimally)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Assessment extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+    this.interval = setInterval(this.fetchData.bind(this), 60000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  fetchData() {
+    fetch('https://onedrop.today/products.json')
+      .then(res => res.json())
+      .then(json => {
+        // Exclude products where the first variant price is 0.00
+        const products = json.products.filter(product => parseFloat(product.variants[0].price) > 0)
+
+        // Custom sort method for descending prices
+        // Based on the first variant's price
+        products.sort((a, b) => parseFloat(b.variants[0].price) - parseFloat(a.variants[0].price))
+
+        this.setState({
+          isLoaded: true,
+          products: products
+        })
+      })
+  }
+
+  render() {
+    const { isLoaded, products } = this.state
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div className="App">
+          <h1>One Drop Assessment — Brandon Goodman</h1>
+          <table className="items-table">
+            <thead>
+              <tr className="items-table__row">
+                <th className="items-table__header">Product Title</th>
+                <th className="items-table__header">Variant Price (descending)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map(product => (
+                <tr key={product.id} className="items-table__row">
+                  <td className="items-table__item">{product.title}</td>
+                  <td className="items-table__item">{product.variants[0].price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
 }
 
-export default App;
+export default Assessment
